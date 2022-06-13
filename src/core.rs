583f4f7613e93deb64c54cc05834bed1d9b4db90
David Cavazos <dcavazos@google.com>
2022-06-13 09:22:06 -0700

    rules simplification

--- src/core.rs
+++ src/core.rs
@@ -6,16 +6,16 @@ use std::rc::Rc;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expr {
-  Nil,                              // Empty environment       _
+  Nil,                              // Empty expression
   Int(i64),                         // Integer value           42
   Ctr(usize),                       // Constructor index       #0
   Var(usize),                       // DeBruijn variable       %0
+  Def(Rc<Expr>, Rc<Expr>),          // Definition              {a, b}
   App(Rc<Expr>, Rc<Expr>),          // Application             a b
-  Lam(Rc<Expr>, Rc<Expr>),          // Lamda closure           \env body
-  Def(Rc<Expr>, Rc<Expr>),          // Environment definition  {a, b}
-  Jmp(Rc<Expr>, Rc<Vec<Rc<Expr>>>), // Lamda closure           ^env [a, b]
-  Fix,                              // Fixed point recursion   @
+  Lam(Rc<Expr>, Rc<Expr>),          // Lamda closure           \a. b
+  Jmp(Rc<Expr>, Rc<Vec<Rc<Expr>>>), // Lamda closure           #a. [b1 | b2 | ..]
   Op2(BinaryOp),                    // Binary operator         +
+  Fix,                              // Fixed point recursion   @
 }
 use Expr::*;
 
@@ -39,32 +39,22 @@ fn var(i: usize) -> Rc<Expr> {
 fn int(i: i64) -> Rc<Expr> {
   Rc::new(Int(i))
 }
-fn lam(env: Rc<Expr>, body: Rc<Expr>) -> Rc<Expr> {
-  Rc::new(Lam(env, body))
-}
 fn def(a: Rc<Expr>, b: Rc<Expr>) -> Rc<Expr> {
   Rc::new(Def(a, b))
 }
 fn app(a: Rc<Expr>, b: Rc<Expr>) -> Rc<Expr> {
   Rc::new(App(a, b))
 }
+fn lam(env: Rc<Expr>, body: Rc<Expr>) -> Rc<Expr> {
+  Rc::new(Lam(env, body))
+}
 fn jmp(env: Rc<Expr>, cases: Rc<Vec<Rc<Expr>>>) -> Rc<Expr> {
   Rc::new(Jmp(env, cases))
 }
 macro_rules! jmp {
-  // () => {
-  //   Rc::new(Jmp(Rc::new(vec![])))
-  // };
-  // ($($x:expr),+ $(,)?) => {
-  //   Rc::new(Jmp(Rc::new(vec![$($x),+])))
-  // };
   ($env: expr, $cases: expr) => {
     Rc::new(Jmp($env, Rc::new($cases.to_vec())))
   };
-}
-
-fn fix(a: Rc<Expr>) -> Rc<Expr> {
-  app(Rc::new(Fix), a)
 }
 fn add(a: Rc<Expr>, b: Rc<Expr>) -> Rc<Expr> {
   app(app(Rc::new(Op2(Add)), a), b)
@@ -75,60 +65,30 @@ fn sub(a: Rc<Expr>, b: Rc<Expr>) -> Rc<Expr> {
 fn mul(a: Rc<Expr>, b: Rc<Expr>) -> Rc<Expr> {
   app(app(Rc::new(Op2(Mul)), a), b)
 }
-
-// TODO: unify Lam, Jmp, Fix
-fn reduce(expr: Rc<Expr>) -> Rc<Expr> {
-  match expr.as_ref() {
-    App(fun, arg) => {
-      let fun = reduce(fun.clone());
-      let arg = reduce(arg.clone());
-      match (fun.as_ref(), arg.as_ref()) {
-        (Nil, _) => reduce(arg),
-        (Var(_), _) => app(fun, arg),
-        (App(op, a), _) => match (op.as_ref(), a.as_ref(), arg.as_ref()) {
-          (Op2(Add), Int(a), Int(b)) => int(a + b),
-          (Op2(Sub), Int(a), Int(b)) => int(a - b),
-          (Op2(Mul), Int(a), Int(b)) => int(a * b),
-          _ => app(fun, arg),
-        },
-        (Lam(env, body), _) => reduce(app(def(arg, env.clone()), body.clone())),
-        (Def(x, _), Var(0)) => reduce(x.clone()),
-        (Def(_, env), Var(i)) => reduce(app(env.clone(), var(i - 1))),
-        (Def(_, _), Lam(env, body)) => lam(app(fun, env.clone()), body.clone()),
-        (Def(_, _), Def(x, env)) => def(app(fun.clone(), x.clone()), app(fun, env.clone())),
-        (Def(_, _), App(a, b)) => reduce(app(
-          reduce(app(fun.clone(), a.clone())),
-          reduce(app(fun.clone(), b.clone())),
-        )),
-        (Def(_, _), Jmp(env, cases)) => jmp(app(fun, env.clone()), cases.clone()), // TODO: add tests
-        (Def(_, _), _) => arg,
-        (Jmp(env, cases), Ctr(i)) => reduce(app(env.clone(), cases[*i].clone())),
-        (Fix, Lam(env, body)) => reduce(app(def(fix(arg.clone()), env.clone()), body.clone())),
-        _ => expr,
-      }
-    }
-    _ => expr,
-  }
+fn fix(a: Rc<Expr>) -> Rc<Expr> {
+  app(Rc::new(Fix), a)
 }
 
 impl Expr {
   fn show(&self) -> String {
     match self {
-      Nil => String::from("_"),
+      Nil => String::from(""),
       Int(i) => i.to_string(),
       Ctr(i) => format!("#{i}"),
       Var(i) => format!("%{i}"),
-      App(fun, arg) => format!("({} {})", fun.show(), arg.show()),
-      Lam(env, body) => format!("(\\{} {})", env.show(), body.show()),
-      Def(x, env) => format!("{{{} {}}}", x.show(), env.show()),
-      Jmp(env, cases) => format!(
-        "{} [{}]",
-        env.show(),
-        cases
-          .iter()
+      Def(a, b) => match b.as_ref() {
+        Nil => format!("{{{}}}", a.show()),
+        _ => format!("{{{}, {}}}", a.show(), b.show()),
+      },
+      App(a, b) => format!("({} {})", a.show(), b.show()),
+      Lam(a, b) => format!("\\{}. {}", a.show(), b.show()),
+      Jmp(a, b) => format!(
+        "#{}. [{}]",
+        a.show(),
+        b.iter()
           .map(|br| br.show())
           .collect::<Vec<String>>()
-          .join(", ")
+          .join(" | ")
       ),
       Fix => String::from("@"),
       Op2(Add) => String::from("+"),
@@ -138,9 +98,60 @@ impl Expr {
   }
 }
 
+fn reduce(expr: Rc<Expr>) -> Rc<Expr> {
+  match expr.as_ref() {
+    App(a, b) => {
+      let a = reduce(a.clone());
+      let b = reduce(b.clone());
+      match (a.as_ref(), b.as_ref()) {
+        (Nil, _) => reduce(b),
+        (Var(_), _) => app(a, b),
+        (Def(a1, _), Var(0)) => reduce(a1.clone()),
+        (Def(_, a2), Var(i)) => reduce(app(a2.clone(), var(i - 1))),
+        (Def(_, _), Lam(b1, b2)) => lam(app(a, b1.clone()), b2.clone()),
+        (Def(_, _), Def(b1, b2)) => def(app(a.clone(), b1.clone()), app(a, b2.clone())),
+        (Def(_, _), App(b1, b2)) => reduce(app(
+          reduce(app(a.clone(), b1.clone())),
+          reduce(app(a.clone(), b2.clone())),
+        )),
+        (Def(_, _), Jmp(b1, b2)) => jmp(app(a, b1.clone()), b2.clone()),
+        (Def(_, _), _) => b,
+        (App(op, i), j) => match (op.as_ref(), i.as_ref(), j) {
+          (Op2(Add), Int(i), Int(j)) => int(i + j),
+          (Op2(Sub), Int(i), Int(j)) => int(i - j),
+          (Op2(Mul), Int(i), Int(j)) => int(i * j),
+          _ => app(a, b),
+        },
+        (Lam(a1, a2), _) => reduce(app(def(b, a1.clone()), a2.clone())),
+        (Jmp(a1, a2), Ctr(i)) => reduce(app(a1.clone(), a2[*i].clone())),
+        (Fix, Lam(b1, b2)) => reduce(app(def(fix(b.clone()), b1.clone()), b2.clone())),
+        _ => expr,
+      }
+    }
+    _ => expr,
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn test_show() {
+    assert_eq!(nil().show(), "");
+    assert_eq!(int(1).show(), "1");
+    assert_eq!(ctr(0).show(), "#0");
+    assert_eq!(var(0).show(), "%0");
+    assert_eq!(def(int(1), nil()).show(), "{1}");
+    assert_eq!(def(int(1), def(int(2), nil())).show(), "{1, {2}}");
+    assert_eq!(app(ctr(0), int(1)).show(), "(#0 1)");
+    assert_eq!(lam(nil(), int(1)).show(), "\\. 1");
+    assert_eq!(lam(def(int(1), nil()), int(2)).show(), "\\{1}. 2");
+    assert_eq!(jmp!(nil(), []).show(), "#. []");
+    assert_eq!(jmp!(nil(), [int(1)]).show(), "#. [1]");
+    assert_eq!(jmp!(nil(), [int(1), int(2)]).show(), "#. [1 | 2]");
+    assert_eq!(jmp!(def(int(1), nil()), []).show(), "#{1}. []");
+  }
 
   // TODO: Break up into individual rules.
   #[test]
@@ -212,21 +223,6 @@ mod tests {
     // {x=1, f=@...} x
     // 1
     assert_eq!(reduce(app(fix(f.clone()), int(1))), int(1));
-  }
-
-  #[test]
-  fn test_show() {
-    assert_eq!(nil().show(), "_");
-    assert_eq!(int(1).show(), "1");
-    assert_eq!(ctr(0).show(), "#0");
-    assert_eq!(var(0).show(), "%0");
-    assert_eq!(app(nil(), int(1)).show(), "(_ 1)");
-    assert_eq!(lam(nil(), int(1)).show(), "(\\_ 1)");
-    assert_eq!(def(nil(), int(1)).show(), "{_ 1}");
-    assert_eq!(jmp!(nil(), []).show(), "_ []");
-    // assert_eq!(jmp!(nil(), [int(1)]).show(), "[1]");
-    // assert_eq!(jmp!(nil(), [int(1), int(2)]).show(), "[1, 2]");
-    // assert_eq!(jmp!(nil(), [int(1), int(2), int(3)]).show(), "[1, 2, 3]");
   }
 
   // TODO: Make these part of each rule tests.
@@ -334,5 +330,8 @@ mod tests {
   fn test_factorial() {
     // f 0 = 1
     // f n = n * f (n - 1)
+    //
+    // f = @ (λf. (0 -> 1 | n -> n * f (n - 1)))
+    // f = @ (λ. #{%0}. [1 | %0 * %1 (- %0 1)])
   }
 }
